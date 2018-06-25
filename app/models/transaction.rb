c5308f17667d758109b17a4a964bc361ec03ee21
shidetake <shidetake@gmail.com>
2018-06-25 22:38:46 +0900

    modify transaction view

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -5,4 +5,8 @@ class Transaction < ApplicationRecord
   belongs_to :source
   belongs_to :store
   belongs_to :wallet
+
+  def parent_category_id
+    category.parent.nil? ? category_id : category.parent.id
+  end
 end
