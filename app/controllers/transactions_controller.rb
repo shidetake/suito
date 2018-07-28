22033d87dbba68f6d15e6f27d19e910a5905198c
shidetake <shidetake@gmail.com>
2018-07-28 16:07:19 +0900

    add save button

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,2 +1,27 @@
 class TransactionsController < ApplicationController
+  def update
+    user = User.find(params[:id])
+    transaction = Transaction.find(params[:transaction][:id])
+
+    if transaction.update_attributes(transaction_params)
+      flash[:success] = "Transaction updated"
+      redirect_to user
+    else
+      #flash[:danger] = "Transaction updated"
+      redirect_to user
+    end
+  end
+
+  private
+
+    def transaction_params
+      params.require(:transaction).permit(:created_at,
+                                          :category_id,
+                                          :store_id,
+                                          :content,
+                                          :yen,
+                                          :source_id,
+                                          :group_id,
+                                          :memo)
+    end
 end
