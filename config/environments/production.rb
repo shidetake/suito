80b449bd0a9c9156aeb4002d4c9b113d3e40ea4d
shidetake <shidetake@gmail.com>
2018-09-17 23:40:33 +0900

    change sendgrid url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://safe-thicket-81226.herokuapp.com'
+  host = 'https://kakeibo.shidetake.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
