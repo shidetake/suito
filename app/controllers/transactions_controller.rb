3e523ad08f1efe681ccebd77ae90174c557e7087
shidetake <shidetake@gmail.com>
2018-11-30 23:27:18 +0900

    rubocop

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -6,7 +6,7 @@ class TransactionsController < ApplicationController
   def create
     @transaction = current_user.transactions.build(transaction_params)
     if @transaction.save
-      flash[:success] = "Transaction created!"
+      flash[:success] = 'Transaction created!'
       redirect_to current_user
     else
       render 'new'
@@ -19,29 +19,25 @@ class TransactionsController < ApplicationController
     params[:transaction][:store_id] = Store.find_by(name: params[:transaction][:store_name])&.id
 
     parent_category_id = Category.find_by(name: params[:transaction][:parent_category_name])&.id
-    params[:transaction][:category_id] = Category.find_by(name: params[:transaction][:category_name], parent_id: parent_category_id)&.id
+    params[:transaction][:category_id] = Category.find_by(name: params[:transaction][:category_name],
+                                                          parent_id: parent_category_id)&.id
 
-    if transaction.update_attributes(transaction_params)
-      flash[:success] = "Transaction updated"
-      redirect_to current_user
-    else
-      #flash[:danger] = "Transaction updated"
-      redirect_to current_user
-    end
+    flash[:success] = 'Transaction updated' if transaction.update_attributes(transaction_params)
+    redirect_to current_user
   end
 
   private
 
-    def transaction_params
-      params.require(:transaction).permit(:valid_record,
-                                          :created_at,
-                                          :category_id,
-                                          :store_id,
-                                          :content,
-                                          :yen,
-                                          :source_id,
-                                          :group_id,
-                                          :wallet_id,
-                                          :memo)
-    end
+  def transaction_params
+    params.require(:transaction).permit(:valid_record,
+                                        :created_at,
+                                        :category_id,
+                                        :store_id,
+                                        :content,
+                                        :yen,
+                                        :source_id,
+                                        :group_id,
+                                        :wallet_id,
+                                        :memo)
+  end
 end
