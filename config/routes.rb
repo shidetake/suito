b96f11a5d50970a186e3dcd9247f18306cb30827
shidetake <shidetake@gmail.com>
2019-03-10 18:34:54 +0900

    cosmetic change

--- config/routes.rb
+++ config/routes.rb
@@ -15,5 +15,5 @@ Rails.application.routes.draw do
   resources :users
   resources :transactions
   resources :account_activations, only: [:edit]
-  resources :password_resets, only:[:new, :create, :edit, :update]
+  resources :password_resets, only: [:new, :create, :edit, :update]
 end
